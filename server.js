686945ebd4ee4e45be8fad816dc1ff39b784fa40
Rebecca Lai <becdotlai@gmail.com>
2016-07-28 21:34:00 -0700

    Fix folder name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http_auth = require('express-http-auth');
 var app = express();
 
 // Serve static files as default
-app.get('/*', express.static(__dirname + '/myprototype.framer'));
+app.get('/*', express.static(__dirname + '/Timeline_Expand_Collapse.framer'));
 
 // Listen for both Heroku and local. Access locally as http://localhost:3333
 app.listen(process.env.PORT || 3333);
